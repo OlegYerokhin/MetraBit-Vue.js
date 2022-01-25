4850b6ea0d08f326c1a1469c825adfb813de93ee
OlegYerokhin <olegyerokhin91@gmail.com>
2022-01-25 21:59:39 +0200

    Vue homework 5

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <AppButton 
+      appButtonText = Save
+    />
+    <PostList />
+    
+  </div>
+</template>
+
+<script>
+import PostList from './components/PostList.vue'
+import AppButton from './components/AppButton.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PostList,
+    AppButton,
+  },
+  data: () => ({
+
+  }),
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/AppButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="app-button">
+    <button
+      class="app-button__btn" 
+      @click="$emit('appButtonClick')"
+      
+    >{{ appButtonText }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppButton',
+  props: ['appButtonText'],
+}
+</script>
+
+<style lang="scss" scoped>
+  .app-button {
+    &__btn {
+      cursor: pointer;
+      border: 2px solid #000;
+      border-radius: 50%;
+      background-color: #ffc400;
+      font-size: 16px;
+      font-weight: 700;
+      width: 55px;
+      height: 55px;
+    }
+  }
+</style>
